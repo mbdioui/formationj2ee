0277c34be861f0163f3107abba4383c47e425928
mohamed salah bdioui <mohamed.bdioui@utbm.fr>
2017-05-30 10:13:11 +0200

    client annotation

--- src/main/java/entity/Client.java
+++ src/main/java/entity/Client.java
@@ -1,7 +1,14 @@
 package entity;
 
-public class Client {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Client {
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Integer ID_Client;
 	private String LASTNAME;
 	private String FIRSTNAME;
@@ -69,7 +76,6 @@ public class Client {
 	}
 	public Client() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 	
 	
